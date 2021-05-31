48cc8b813b6866b5ee589a6d5bce303b94754709
Waleed-Asim <83092895+Waleed-Asim@users.noreply.github.com>
2021-06-01 02:25:50 +0500

    Add Html and Js files

---
+++ app.js
@@ -0,0 +1,69 @@
+// *********************** 17 - 20 ************************
+
+// Q1  
+
+// var arr = [""],[""];
+
+
+// Q2 
+
+// Q3
+
+// for(var i = 1; i <= 10; i++){
+//     document.write(i + "</br>")
+// }
+
+// Q4
+
+// var table = +prompt("Enter a number to show its table");
+
+// for(var i = 1; i <= 10; i++){
+//     document.write(table + " x " + i + " = " + table*i + "</br>");
+// }
+
+
+
+// Q5
+
+// var arr = ["apple","bannana","mango","strawberry","orange"]
+
+// for(var i = 0; i < 5; i++){
+//     document.write(arr[i] + "</br>");
+// }
+
+
+// Q6
+
+// var arr = [1,2,3,4,5,6,7,8,9,10]
+
+// for(var i = 1; i <= 10; i++){
+//     document.write(i + ",")
+// }
+
+
+// Q7
+
+// Q8
+
+// Q9
+
+// Q10
+
+// for(var i = 1; i <= 100; i++){
+//     if(i%5 === 0){
+//         document.write(i + ",")
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
